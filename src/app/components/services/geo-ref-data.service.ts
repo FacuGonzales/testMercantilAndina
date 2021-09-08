e43fbd39b246b80e2aeb088d148ab7f4dce7154a
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-09-08 17:30:48 -0300

    Se arman metodos para obtener pronvincias y localidades

--- src/app/components/services/geo-ref-data.service.ts
+++ src/app/components/services/geo-ref-data.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GeoRefDataService {
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
+
+  obtenerProvincias(): Observable<any>{
+    let url = `https://apis.datos.gob.ar/georef/api/provincias`;
+
+    let headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+    return this.http.get(url, {headers: headers});
+  }
+
+  obtenerCiudades(idProvincia: number, ): Observable<any>{
+    let url = `https://apis.datos.gob.ar/georef/api/municipios?provincia=${idProvincia}&campos=id,nombre`;
+
+    let headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+    return this.http.get(url, {headers: headers});
+  }
+  
 }
