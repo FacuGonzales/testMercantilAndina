1eb969cb7db2dbd2b64252c3f1e2e4bde05fef69
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-09-08 11:08:31 -0300

    Se crea module para shared

---
+++ src/app/components/components-shared.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DatosPersonalesComponent } from './shared/datos-personales/datos-personales.component';
+
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+    ],
+    declarations: [
+        
+        DatosPersonalesComponent
+    ],
+    exports: [
+    ],
+    providers: [],
+})
+
+export class ComponentsSharedModule { }
