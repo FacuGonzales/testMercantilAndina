071f040480d1a18f3d57f61cc3ac3fe27aa4ab84
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-09-08 18:50:06 -0300

    Se arma metodo para obtener el usuario valido

--- src/app/components/services/usuario-data.service.ts
+++ src/app/components/services/usuario-data.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioDataService {
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
+
+  getUsuarioValid(usuario: string, ): Observable<any>{
+    let url = `https://servicios.qamercantilandina.com.ar/api_mock_frontend/v1/usuarios?nombre=${usuario}`;
+
+    let headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+    return this.http.get(url, {headers: headers});
+  }
+  
 }
