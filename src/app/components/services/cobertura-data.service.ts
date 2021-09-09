a6d07b87962c01a6aaeebd69e614d213136a10c4
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-09-09 13:19:34 -0300

    Se arma metodo para obtener coberturas

--- src/app/components/services/cobertura-data.service.ts
+++ src/app/components/services/cobertura-data.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoberturaDataService {
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
+
+  getCoberturas(): Observable<any>{
+    let url = `https://servicios.qamercantilandina.com.ar/api_mock_frontend/v1/coberturas`
+
+    let headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+    return this.http.get(url, {headers: headers});
+  }
+  
 }
