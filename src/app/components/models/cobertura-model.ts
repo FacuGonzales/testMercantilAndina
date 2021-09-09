6d6fb19525cfae785628e05412cb4acb1644043c
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-09-09 08:59:57 -0300

    Se crea modelo de cobertura

---
+++ src/app/components/models/cobertura-model.ts
@@ -0,0 +1,12 @@
+export interface ICoberturaModel {
+    numero: number,
+    costo: number,
+    producto: string,
+    texto: string,
+    franquicia: string,
+    codigoProducto: number,
+    titulo: string,
+    descripcion: string,
+    puntaje: number,
+    granizo: boolean
+}
