80b6ed189b6a11a402955e047f9de5248b030ef3
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-09-08 22:27:31 -0300

    Se arma metodos para services

--- src/app/components/services/vehiculo-data.service.ts
+++ src/app/components/services/vehiculo-data.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VehiculoDataService {
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
+
+  getInfoVehiculos( codigoMarca?: number, anio?: number, version?: string ): Observable<any>{
+    // obtenerMarcas
+    let url = `https://servicios.qamercantilandina.com.ar/api/v1/vehiculos/marcas`
+
+    // obtenerModelos
+    if(codigoMarca && anio) url = `https://servicios.qamercantilandina.com.ar/api/v1/vehiculos/marcas/${codigoMarca}/${anio}`;
+
+    // obtenerVersiones
+    if(codigoMarca && anio && version) url = `https://servicios.qamercantilandina.com.ar/api/v1/vehiculos/marcas/${codigoMarca}/${anio}/${version}`;
+
+    let headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+    return this.http.get(url, {headers: headers});
+  }
+  
 }
