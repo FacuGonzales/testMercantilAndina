e30d7555f905c8fb9d0bdd6c95966bfec7215f35
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-09-09 13:18:23 -0300

    Se arma funcionalidad para prdenar de manera decreciente

--- src/app/components/shared/utils/util.ts
+++ src/app/components/shared/utils/util.ts
@@ -1,5 +1,11 @@
-export const alphaOrder = (a,b) => {
-    if(a < b) return -1;
-    if(a > b) return 1;
+export const alphaOrder = (a,b, decrec?:boolean) => {
+  if(decrec){
+    if(b < a) return -1;
+    if(b > a) return 1;
     return 0;
   }
+  
+  if(a < b) return -1;
+  if(a > b) return 1;
+  return 0;
+}
