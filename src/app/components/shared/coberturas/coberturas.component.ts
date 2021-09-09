d2a3317d8656dada3d8f1f4a9bf55a32a4172d9b
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-09-09 08:56:43 -0300

    Se crea componente de coberturas

---
+++ src/app/components/shared/coberturas/coberturas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-coberturas',
+  templateUrl: './coberturas.component.html',
+  styleUrls: ['./coberturas.component.scss']
+})
+export class CoberturasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
