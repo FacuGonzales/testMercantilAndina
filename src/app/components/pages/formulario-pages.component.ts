cb2a94f71263a917ca26b256a12dba23cce7f8ce
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-09-09 18:30:51 -0300

    Se arma formulario component

--- src/app/components/pages/formulario-pages.component.ts
+++ src/app/components/pages/formulario-pages.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { setStorage } from '../shared/utils/util'; 
 
 @Component({
   selector: 'app-formulario-pages',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioPagesComponent implements OnInit {
 
+  datosPersonalesForm: FormControl;
+  datosVehiculoForm: FormControl;
+  datosCoberturaForm: FormControl;
+
+  registroFinalizado: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.datosPersonalesForm = new FormControl('', Validators.required);
+    this.datosVehiculoForm = new FormControl('', Validators.required);
+    this.datosCoberturaForm = new FormControl('', Validators.required);
+  }
+
+  loadDatosPersonales(value){
+    this.datosPersonalesForm = value ? value : '';
+
+    setStorage('datos-personales', this.datosPersonalesForm);
+  }
+
+  loadDatosVehiculo(value){
+    this.datosVehiculoForm = value ? value : '';
+
+    setStorage('datos-vehiculo', this.datosVehiculoForm);
+  }
+
+  coberturaSelected(value){
+    this.datosCoberturaForm = value ? value : '';
+
+    setStorage('datos-cobertura', this.datosCoberturaForm);
+
+    this.registroFinalizado = true;
   }
 
 }
